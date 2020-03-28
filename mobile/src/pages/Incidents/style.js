fdd19c300d8f941f48c4c17f57c18767dac10095
Daniel Ferreira <daferreira1996@icloud.com>
2020-03-27 21:22:09 -0300

    Limpando a pasta

--- mobile/src/pages/Incidents/style.js
+++ mobile/src/pages/Incidents/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { Constants } from 'expo-constants';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
     container: {
@@ -7,15 +7,63 @@ export default StyleSheet.create({
         paddingHorizontal: 24,
         paddingTop: Constants.statusBarHeight + 20,
     },
+
     header: {
-        flex: 'row',
+        flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-
     },
     headerText: {
         fontSize: 15,
         color: '#737380',
         
+    },
+    headerTextBold: {
+        fontWeight: 'bold',
+    },
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop: 48,
+        color: '#13131a',
+        fontWeight: 'bold',
+    },
+    description: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#737380',
+    },
+
+    incidentList: {
+        marginTop: 32,
+    },
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 16,
+    },
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+    },
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380',
+    },
+
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    detailsButtonText: {
+        color: '#e02041',
+        fontSize: 15,
+        fontWeight:'bold',
     },
 }); 
