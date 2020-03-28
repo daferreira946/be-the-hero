8e581df4bf6fcdf1b2399b88eafb045be2d0035b
Daniel Ferreira <daferreira1996@icloud.com>
2020-03-28 19:52:00 -0300

    estilização prettier frontend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,54 +1,56 @@
-import React, {useState}  from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiLogIn } from 'react-icons/fi';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 
-import apt from '../../services/api';
+import apt from "../../services/api";
 
-import './style.css';
+import "./style.css";
 
-import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
 
 export default function Logon() {
-const [id, setId] = useState('');
-const history = useHistory();
+  const [id, setId] = useState("");
+  const history = useHistory();
 
-async function handleLogin(e) {
+  async function handleLogin(e) {
     e.preventDefault();
 
     try {
-        const response = await apt.post('sessions', { id });
+      const response = await apt.post("sessions", { id });
 
-        localStorage.setItem('ongId', id);
-        localStorage.setItem('ongName', response.data.name);
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
 
-        history.push('/profile');
+      history.push("/profile");
     } catch (err) {
-        alert('Falha no login, tente novamente.');
+      alert("Falha no login, tente novamente.");
     }
-}
+  }
 
-    return (
-        <div className="logon-container">
-            <section className="form">
-                <img src={logoImg} alt="Be The Hero" />
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
 
-                <form onSubmit={handleLogin}>
-                    <h1>Faça seu Logon</h1>
-                    <input 
-                        placeholder="Sua ID"
-                        value={id}
-                        onChange={e => setId(e.target.value)}
-                    />
-                    <button type="submit" className="button">Entrar</button>
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu Logon</h1>
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+          <button type="submit" className="button">
+            Entrar
+          </button>
 
-                    <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#E02041" />
-                        Não tenho cadastro
-                    </Link>
-                </form>
-            </section>
-            <img src={heroesImg} alt="Heroes" />
-        </div>
-    );
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
